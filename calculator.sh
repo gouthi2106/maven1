82256028236913319835b82878304cfb05a15ca5
Ubuntu <ops@jenkinskt.pl5zjjk3qpqe5pvmcenf3p40za.rx.internal.cloudapp.net>
2018-06-09 09:31:31 +0000

    modified calculator file

--- calculator.sh
+++ calculator.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
-echo " enter the first value"
-read n1
-echo "enter the second value"
-read n2
+echo " enter the values to calculate"
+a=$1
+b=$2
+c=$3
 
-echo "enter the choice:"
-echo "1: add"
-echo "2: sub"
-echo "3: mul"
-
-echo "enter the operand"
-read a
-
-
-if  [ $a -eq 1 ] 
+if  [ $c -eq 1 ] 
 then
-	c=`expr $n1 + $n2`
-	echo "value is $c"
-elif [ $a -eq  2 ] 
+	ans=`expr $a + $b`
+	echo "value is $ans"
+elif [ $c -eq  2 ] 
 then
-	c=`expr $n1 - $n2`
-	echo "value is $c"
-elif [ $a -eq 3 ] 
+	ans=`expr $a - $b`
+	echo "value is $ans"
+elif [ $c -eq 3 ] 
 then
-	c=`expr $n1 \* $n2`
-	echo "the value is $c"
+	ans=`expr $a \* $b`
+	echo "the value is $ans"
 fi
